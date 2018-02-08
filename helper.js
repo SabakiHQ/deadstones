3d24f55157905799a1ee95b7dc6e95d1996c4ecd
Yichuan Shen <shen.yichuan@gmail.com>
2018-02-08 23:55:24 +0100

    Fix helper

--- helper.js
+++ helper.js
@@ -1,2 +1,2 @@
-const equals = v => w => w[0] === v[0] && w[1] === v[1]
-const equalsSign = (board, ...s) => v => s.includes(board.get(v))
+exports.equals = v => w => w[0] === v[0] && w[1] === v[1]
+exports.equalsSign = (board, ...s) => v => s.includes(board.get(v))
