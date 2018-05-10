c1dcc174d58017c07a9b0b93dbd4aebf7bbe8a1c
Yichuan Shen <shen.yichuan@gmail.com>
2018-05-10 23:12:01 +0200

    Add lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,36 @@ mod vertex;
 mod pseudo_board;
 mod deadstones;
 
+use pseudo_board::*;
+
 #[wasm_bindgen]
-pub fn get_floating_stones(input: &str) -> String {
-    String::new()
+pub fn guess(data: &str, finished: bool, iterations: usize) -> String {
+    let data: BoardData = serde_json::from_str(data).unwrap();
+    let result = deadstones::guess(data, finished, iterations);
+
+    serde_json::to_string(&result).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn get_probability_map(data: &str, iterations: usize) -> String {
+    let data: BoardData = serde_json::from_str(data).unwrap();
+    let result = deadstones::get_probability_map(data, iterations);
+
+    serde_json::to_string(&result).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn play_till_end(data: &str, sign: i8) -> String {
+    let data: BoardData = serde_json::from_str(data).unwrap();
+    let result = deadstones::play_till_end(data, sign);
+
+    serde_json::to_string(&result).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn get_floating_stones(data: &str) -> String {
+    let data: BoardData = serde_json::from_str(data).unwrap();
+    let result = deadstones::get_floating_stones(data);
+
+    serde_json::to_string(&result).unwrap()
 }
