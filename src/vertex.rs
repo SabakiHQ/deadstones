94d9fbecc24d4fbe961ab0be995a25737e80c72a
Yichuan Shen <shen.yichuan@gmail.com>
2018-05-10 17:13:18 +0200

    Add vertex module

---
+++ src/vertex.rs
@@ -0,0 +1,8 @@
+pub type Sign = i8;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Vertex(pub usize, pub usize);
+
+pub fn get_neighbors(Vertex(x, y): Vertex) -> Vec<Vertex> {
+    vec![Vertex(x - 1, y), Vertex(x + 1, y), Vertex(x, y - 1), Vertex(x, y + 1)]
+}
