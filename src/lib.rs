6cb5127609aeddd1fb35c78ef8ae74d188416575
Yichuan Shen <shen.yichuan@gmail.com>
2018-05-12 21:53:50 +0200

    Don't call Date.now() from Rust side

--- src/lib.rs
+++ src/lib.rs
@@ -12,33 +12,27 @@ use rand::Rand;
 use pseudo_board::*;
 
 #[wasm_bindgen]
-extern {
-    #[wasm_bindgen(js_namespace = Date)]
-    fn now() -> u32;
-}
-
-#[wasm_bindgen]
-pub fn guess(data: Vec<Sign>, width: usize, finished: bool, iterations: usize) -> Vec<i32> {
+pub fn guess(data: Vec<Sign>, width: usize, finished: bool, iterations: usize, seed: u32) -> Vec<i32> {
     let board = PseudoBoard::new(data, width);
 
-    deadstones::guess(board, finished, iterations, &mut Rand::new(now()))
+    deadstones::guess(board, finished, iterations, &mut Rand::new(seed))
     .into_iter()
     .map(|x| x as i32)
     .collect()
 }
 
 #[wasm_bindgen]
-pub fn get_probability_map(data: Vec<Sign>, width: usize, iterations: usize) -> Vec<f32> {
+pub fn get_probability_map(data: Vec<Sign>, width: usize, iterations: usize, seed: u32) -> Vec<f32> {
     let board = PseudoBoard::new(data, width);
     
-    deadstones::get_probability_map(board, iterations, &mut Rand::new(now()))
+    deadstones::get_probability_map(board, iterations, &mut Rand::new(seed))
 }
 
 #[wasm_bindgen]
-pub fn play_till_end(data: Vec<Sign>, width: usize, sign: Sign) -> Vec<Sign> {
+pub fn play_till_end(data: Vec<Sign>, width: usize, sign: Sign, seed: u32) -> Vec<Sign> {
     let board = PseudoBoard::new(data, width);
 
-    deadstones::play_till_end(board, sign, &mut Rand::new(now())).data
+    deadstones::play_till_end(board, sign, &mut Rand::new(seed)).data
 }
 
 #[wasm_bindgen]
--- src/main.js
+++ src/main.js
@@ -11,21 +11,21 @@ const parseGrid = (values, width) => [...Array(values.length / width)]
 
 exports.guess = function(data, {finished = false, iterations = 100} = {}) {
     let [newData, width] = parseBoard(data)
-    let indices = lib.guess(newData, width, finished, iterations)
+    let indices = lib.guess(newData, width, finished, iterations, Date.now())
 
     return parseVertices(indices, width)
 }
 
 exports.playTillEnd = function(data, sign) {
     let [newData, width] = parseBoard(data)
-    let values = lib.play_till_end(newData, width, sign)
+    let values = lib.play_till_end(newData, width, sign, Date.now())
 
     return parseGrid(values, width)
 }
 
 exports.getProbabilityMap = function(data, iterations) {
     let [newData, width] = parseBoard(data)
-    let values = lib.get_probability_map(newData, width, iterations)
+    let values = lib.get_probability_map(newData, width, iterations, Date.now())
 
     return parseGrid(values, width)
 }
