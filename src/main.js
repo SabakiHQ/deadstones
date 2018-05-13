f9b3021d8052a8600e78f3412e6c2da03f4be123
Yichuan Shen <shen.yichuan@gmail.com>
2018-05-13 11:07:10 +0200

    Files restructure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-const lib = require('../wasm/deadstones')
-
 const parseBoard = data => [
     data.reduce((acc, x) => (acc.push(...x), acc), []),
     data.length > 0 ? data[0].length : 0
@@ -16,30 +14,36 @@ const parseGrid = (values, width) => {
         .map((_, y) => values.slice(y * width, (y + 1) * width))
 }
 
-exports.guess = function(data, {finished = false, iterations = 100} = {}) {
-    let [newData, width] = parseBoard(data)
-    let indices = lib.guess(newData, width, finished, iterations, Date.now())
+module.exports = function(lib) {
+    let result = {}
 
-    return parseVertices(indices, width)
-}
+    result.guess = function(data, {finished = false, iterations = 100} = {}) {
+        let [newData, width] = parseBoard(data)
+        let indices = lib.guess(newData, width, finished, iterations, Date.now())
 
-exports.playTillEnd = function(data, sign) {
-    let [newData, width] = parseBoard(data)
-    let values = lib.play_till_end(newData, width, sign, Date.now())
+        return parseVertices(indices, width)
+    }
 
-    return parseGrid(values, width)
-}
+    result.playTillEnd = function(data, sign) {
+        let [newData, width] = parseBoard(data)
+        let values = lib.play_till_end(newData, width, sign, Date.now())
 
-exports.getProbabilityMap = function(data, iterations) {
-    let [newData, width] = parseBoard(data)
-    let values = lib.get_probability_map(newData, width, iterations, Date.now())
+        return parseGrid(values, width)
+    }
 
-    return parseGrid(values, width)
-}
+    result.getProbabilityMap = function(data, iterations) {
+        let [newData, width] = parseBoard(data)
+        let values = lib.get_probability_map(newData, width, iterations, Date.now())
 
-exports.getFloatingStones = function(data) {
-    let [newData, width] = parseBoard(data)
-    let indices = lib.get_floating_stones(newData, width)
+        return parseGrid(values, width)
+    }
 
-    return parseVertices(indices, width)
+    result.getFloatingStones = function(data) {
+        let [newData, width] = parseBoard(data)
+        let indices = lib.get_floating_stones(newData, width)
+
+        return parseVertices(indices, width)
+    }
+
+    return result
 }
