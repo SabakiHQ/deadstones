1b3b938120aa54b9b64dfc17310672fb8f4370eb
Harsh Pandey <hpandey793@gmail.com>
2022-07-10 10:56:18 -0400

    try and add ESM support

--- js/wasm.js
+++ js/wasm.js
@@ -1,3 +1,10 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const _dirname = typeof __dirname !== 'undefined'
+  ? __dirname
+  : dirname(fileURLToPath(import.meta.url));
+
 const make = wasm => {
     let result = {}
 
